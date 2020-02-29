bf0daad0b35c3ceba3735fa14b3b90244a00359d
natmaitre <natanvenierfr@gmail.com>
2020-02-28 22:09:26 -0600

    fix an error in script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,10 @@
 var lang = "en";
 if (lang === "en") document.getElementById("home_title").innerHTML = "Home";
 
-var lang = "en";
 if (lang === "en") document.getElementById("games_title").innerHTML = "Online Games";
 
-var lang = "en";
 if (lang === "en") document.getElementById("meme_title").innerHTML = "Watch Memes";
 
-var lang = "en";
 if (lang === "en") document.getElementById("learn_title").innerHTML = "Home";
 
-var lang = "en";
-if (lang === "en") document.getElementById("title_Nat1").innerHTML = "Natmaitre";
-
-var lang = "en";
 if (lang === "en") document.getElementById("desc_home").innerHTML = "Hello, Welcome to my wed site";

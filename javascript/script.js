b4b7cd5ea1d4bd1349c9516c569a271aaa94ae3b
natmaitre <natanvenierfr@gmail.com>
2020-02-29 11:24:17 -0600

    add translate in english

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,6 @@ if (lang === "en") document.getElementById("games_title").innerHTML = "Online Ga
 
 if (lang === "en") document.getElementById("meme_title").innerHTML = "Watch Memes";
 
-if (lang === "en") document.getElementById("learn_title").innerHTML = "Home";
+if (lang === "en") document.getElementById("learn_title").innerHTML = "Learn";
 
-if (lang === "en") document.getElementById("desc_home").innerHTML = "Hello, Welcome to my wed site";
+if (lang === "en") document.getElementById("desc_home").innerHTML = "<center>Hello and Welcome to my web site Natmaitr.<br>What is the web site Natmaitre ?<br><br>The web site Natmaitre, its a web site were you can play games online or watch meme...</br>Thank-you for using my web site !!</center>";

fe5ab4bfa7e29ce861d05ee3cd0626a7e73e6059
natmaitre <natanvenierfr@gmail.com>
2020-06-11 11:50:37 -0400

    remove meme file

--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,7 @@ if (lang === "en") document.getElementById("home_title").innerHTML = "Home";
 
 if (lang === "en") document.getElementById("games_title").innerHTML = "Online Games";
 
-if (lang === "en") document.getElementById("meme_title").innerHTML = "Watch Memes";
 
 if (lang === "en") document.getElementById("learn_title").innerHTML = "Learn";
 
 if (lang === "en") document.getElementById("desc_home").innerHTML = "<center>Hello and Welcome to my web site Natmaitr.<br>What is the web site Natmaitre ?<br><br>The web site Natmaitre, its a web site were you can play games online or watch meme...</br>Thank-you for using my web site !!</center>";
-
-if (lang === "en") document.getElementById("desc_meme").innerHTML = "<center>Hello and  sur l'onglet meme.<br><br>Dans cette onglet tu pourras regarder des memes.<br>Clique sur le boutton avec la langue choisi.";
